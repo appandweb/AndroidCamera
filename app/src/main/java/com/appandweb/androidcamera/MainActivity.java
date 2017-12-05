d62a098dfc04955114d162f5bb3d20c1db32868e
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-12-05 13:56:05 +0100

    Added link to edit in a different location

--- app/src/main/java/com/appandweb/androidcamera/MainActivity.java
+++ app/src/main/java/com/appandweb/androidcamera/MainActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.io.File;
+
+import static com.appandweb.androidcamera.EditPictureActivity.EXTRA_FORCE_FIXED_ASPECT_RATIO;
+import static com.appandweb.androidcamera.EditPictureActivity.EXTRA_PICTURE_FILE;
+import static java.security.AccessController.getContext;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -27,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        findViewById(R.id.btn_edit_picture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchEditPicture();
+            }
+
+        });
     }
 
     protected void launchCamera() {
@@ -36,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
 
     protected void launchLegacyCamera() {
         Intent intent = new Intent(this, LegacyCameraActivity.class);
+        startActivity(intent);
+    }
+
+    protected void launchEditPicture() {
+        File f = new File(this.getExternalFilesDir(null), "pic.jpg");
+        Intent intent = new Intent(this, EditPictureActivity.class);
+        intent.putExtra(EXTRA_PICTURE_FILE, f.getAbsolutePath());
+        intent.putExtra(EXTRA_FORCE_FIXED_ASPECT_RATIO, false);
         startActivity(intent);
     }
 }
